7f52a858fccc76ef8beba509c6c34dad39d79cb5
Brian Bechtel <brianrbechtel@gmail.com>
2017-07-27 19:47:48 -0500

    Added save icon to menu in EditRecording Screen

--- app/src/main/java/cs371m/alarming/EditRecording.java
+++ app/src/main/java/cs371m/alarming/EditRecording.java
@@ -1,7 +1,12 @@
 package cs371m.alarming;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class EditRecording extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class EditRecording extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_recording);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_save, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_save:
+                Intent result = new Intent();
+                /**
+                 * populate result with data to send back
+                 * e.g. result.putExtra(String key, ? data);
+                 */
+                setResult(Activity.RESULT_OK, result);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
