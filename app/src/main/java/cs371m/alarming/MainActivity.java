fb935334a73d8c2a8698e6524e2ba481ce596435
Brian Bechtel <brianrbechtel@gmail.com>
2017-08-05 18:08:04 -0500

    removinf spurious imports

--- app/src/main/java/cs371m/alarming/MainActivity.java
+++ app/src/main/java/cs371m/alarming/MainActivity.java
@@ -17,8 +17,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
